14e35e927e2ec25ade798f120b204c16deeaa3de
Ibrahim Sel <ibrahim.sel@eteration.com>
2025-01-20 15:01:17 +0300

    update package metadata

--- setup.py
+++ setup.py
@@ -1,34 +1,34 @@
-from setuptools import find_packages, setup
 import os
+from setuptools import find_packages, setup
 from glob import glob
 
-PACKAGE_NAME = 'composer'
+PACKAGE_NAME = "composer"
 
 setup(
     name=PACKAGE_NAME,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    version="0.0.0",
+    packages=find_packages(exclude=["test"]),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + PACKAGE_NAME]),
-        ('share/' + PACKAGE_NAME, ['package.xml']),
+        ("share/ament_index/resource_index/packages", ["resource/" + PACKAGE_NAME]),
+        ("share/" + PACKAGE_NAME, ["package.xml"]),
         (os.path.join("share", PACKAGE_NAME, "config"), glob("config/*.yaml")),
         (os.path.join("share", PACKAGE_NAME, "launch"), glob("launch/*.launch.py")),
     ],
-    install_requires=['docker', 'setuptools'],
+    install_requires=["docker", "setuptools", "jsonschema"],
     zip_safe=True,
-    maintainer='composiv.ai',
-    maintainer_email='info@composiv.ai',
-    description='Composer provides orchestration utilites for ROS2 environment',
-    license='Eclipse Public License',
-    tests_require=['unittest'],
-    test_suite='test',
+    maintainer="composiv.ai",
+    maintainer_email="info@composiv.ai",
+    description="Composer provides orchestration utilites for ROS2 environment",
+    license="Eclipse Public License",
+    tests_require=["unittest", "pytest"],
+    test_suite="test",
     entry_points={
-        'console_scripts': [
-            'muto_composer = composer.muto_composer:main',
-            'compose_plugin = composer.plugins.compose_plugin:main',
-            'native_plugin = composer.plugins.native_plugin:main',
-            'launch_plugin = composer.plugins.launch_plugin:main'
+        "console_scripts": [
+            "muto_composer = composer.muto_composer:main",
+            "compose_plugin = composer.plugins.compose_plugin:main",
+            "native_plugin = composer.plugins.native_plugin:main",
+            "launch_plugin = composer.plugins.launch_plugin:main",
+            "daemon = composer.introspection.muto_daemon:main",
         ],
     },
 )
